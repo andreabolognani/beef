99b7339778597f56be0490d57f15e54aed25a8de
Andrea Bolognani <eof@kiyuko.org>
2017-01-11 00:11:55 +0100

    Update copyright years

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /* Beef - Flexible Brainfuck interpreter
- * Copyright (C) 2005-2014  Andrea Bolognani <eof@kiyuko.org>
+ * Copyright (C) 2005-2017  Andrea Bolognani <eof@kiyuko.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
